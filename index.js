e2e85a1a3ef97913ecece583a055176cba13a339
edwin <ngisahedwin@gmail.com>
2022-08-16 12:43:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+const num3 = 16;
+const num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(4, 8, 12, 20);
+console.log(max);
